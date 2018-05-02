42f83fa2ff11d456fd6af122257b3e5d97712c2f
Justin Hays <jhays124@yahoo.com>
2018-05-01 20:54:10 -0700

    sort of works.  commas are being added, but at least things are looping.  Hi Wes.

--- js/index.js
+++ js/index.js
@@ -10,7 +10,29 @@ function handleIncomingFiles(e) {
     reader.readAsText(targetArray[i], 'UTF-8')
     reader.onload = (evt) => {
       let fileArr = JSON.parse(evt.target.result)
-      console.log(fileArr)
+      // console.log(fileArr)
+      let result = ''
+      fileArr.forEach(obj => {
+        result += objHandler(obj)
+      })
+      pageBody.innerHTML = result
     }
   }
+}
+
+
+function objHandler(obj) {
+  if (!obj) return
+  let element = `<${obj.tag}>`
+  if (Array.isArray(obj.content)) {
+    element += obj.content.map(e => objHandler(e))
+  }
+  else if (typeof obj.content === 'object') {
+    element += objHandler(obj.content)
+  }
+  else if (typeof obj.content === 'string') {
+    element += `${obj.content}`
+  }
+  element += `</${obj.tag}>`
+  return element
 }
